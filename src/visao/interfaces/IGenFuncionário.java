8aa7904f3647bd0fa86e49e1efdd1dc91c4d1fb4
Gabriel Borges <gabriel.bgs00@gmail.com>
2019-11-30 23:49:47 -0400

    Update IGenFuncionário.java

--- src/visao/interfaces/IGenFuncionário.java
+++ src/visao/interfaces/IGenFuncionário.java
@@ -3,6 +3,6 @@ package visao.interfaces;
 import modelo.Funcionario;
 
 public interface IGenFuncionário {
-    public void setFuncionario (String nome, char sexo, String endereco, String cidade, String estado, String telefoneResidencial, String telefoneCelular, String email, int dataHora, int id, String cpf);
+    public boolean setFuncionario (String nome, char sexo, String endereco, String cidade, String estado, String telefoneResidencial, String telefoneCelular, String email, int dataHora, int id, String cpf);
     public Funcionario getFuncionario (String nome);
 }
