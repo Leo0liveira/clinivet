37eaad84591f3b0ce96136dba15e1072814cf905
Pedro <pedromvpaes@gmail.com>
2019-12-02 20:07:08 -0400

    Atualiza script bd

--- db.sql
+++ db.sql
@@ -2,22 +2,6 @@ CREATE DATABASE IF NOT EXISTS clinivet;
 CREATE SCHEMA clinivetschema;
 SET search_path='clinivetschema';
 
-TRUNCATE TABLE clinivetschema.administradores;
-TRUNCATE TABLE clinivetschema.animais;
-TRUNCATE TABLE clinivetschema.clientes;
-TRUNCATE TABLE clinivetschema.consultas;
-TRUNCATE TABLE clinivetschema.documentos;
-TRUNCATE TABLE clinivetschema.especies;
-TRUNCATE TABLE clinivetschema.funcionarios;
-TRUNCATE TABLE clinivetschema.pagamentos;
-TRUNCATE TABLE clinivetschema.pedidos;
-TRUNCATE TABLE clinivetschema.procedimentos;
-TRUNCATE TABLE clinivetschema.racas;
-TRUNCATE TABLE clinivetschema.relatorios;
-TRUNCATE TABLE clinivetschema.resultados;
-TRUNCATE TABLE clinivetschema.servicos;
-TRUNCATE TABLE clinivetschema.veterinarios;
-
 CREATE TABLE IF NOT EXISTS clinivetschema.administradores (
 	nome character varying(255) NOT NULL,
 	sexo character(1),
@@ -43,6 +27,7 @@ CREATE TABLE IF NOT EXISTS clinivetschema.funcionarios (
 	telefone_celular character varying(255)  NOT NULL,
 	email character varying(255)  NOT NULL,
 	cpf integer NOT NULL,
+	tipo character varying(255) NOT NULL,
 	id SERIAL NOT NULL PRIMARY KEY,
 
     CONSTRAINT funcionario_sexo_check CHECK ((sexo = ANY (ARRAY['F'::bpchar, 'M'::bpchar])))
@@ -67,9 +52,9 @@ CREATE TABLE IF NOT EXISTS clinivetschema.animais (
 	nome character varying(255) NOT NULL,
 	sexo character(1) NOT NULL,
 	cor character varying(255) NOT NULL,
-	especie character varying(255) NOT NULL,
-	raca character varying(255) NOT NULL,
-	donoId integer NOT NULL,
+	id_especie integer,
+	id_raca integer,
+	id_dono integer NOT NULL,
 	id SERIAL NOT NULL PRIMARY KEY,
 
     CONSTRAINT animal_sexo_check CHECK ((sexo = ANY (ARRAY['F'::bpchar, 'M'::bpchar])))
@@ -95,7 +80,7 @@ CREATE TABLE IF NOT EXISTS clinivetschema.consultas (
 	data_hora timestamp NOT NULL,
 	id SERIAL NOT NULL PRIMARY KEY,
 	id_animal integer NOT NULL,
-	id_veterinario integer NOT NULL,
+	id_veterinario integer NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS clinivetschema.documentos (
@@ -111,27 +96,97 @@ CREATE TABLE IF NOT EXISTS clinivetschema.pagamentos (
 	id SERIAL NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS clinivet.pedidos (
+CREATE TABLE IF NOT EXISTS clinivetschema.pedidos (
 	tipo character varying(255) NOT NULL,
 	data_hora timestamp NOT NULL,
 	id SERIAL NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS clinivet.procedimentos (
+-- 2 GATILHOS A SEREM CRIADOS
+
+CREATE TABLE IF NOT EXISTS clinivetschema.procedimentos (
 	tipo character varying(255) NOT NULL,
 	data_hora timestamp NOT NULL,
 	id SERIAL NOT NULL PRIMARY KEY,
-	responsavel integer NOT NULL
+	responsavel integer
 );
 
-CREATE TABLE IF NOT EXISTS clinivet.racas (
-	nome character varying(255) PRIMARY KEY,
-	descricao character varying(255)
+CREATE TABLE IF NOT EXISTS clinivetschema.servicos (
+	numero integer NOT NULL PRIMARY KEY,
+	tipo character varying(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS clinivet.relatorios (
+CREATE TABLE IF NOT EXISTS clinivetschema.animais_servicos (
+	numero_servico integer NOT NULL,
+	id_animal integer NOT NULL,
+	data_hora timestamp NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.vacinas (
+	nome_vacina character varying(255) NOT NULL,
+	lugar_aplicacao character varying(255) NOT NULL,
+	data_hora timestamp NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	responsavel integer
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.injecoes (
+	nome_injecao character varying(255) NOT NULL,
+	local_aplicacao character varying(255) NOT NULL,
+	data_hora timestamp NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	responsavel integer
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.banhos (
+	nome_produto character varying(255) NOT NULL,
+	data_hora timestamp NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	responsavel integer
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.tosas (
+	data_hora timestamp NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	responsavel integer
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.cirurgias (
+	nome character varying(255) NOT NULL,
+	descricao character varying(255),
+	data_hora timestamp NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	responsavel integer
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.relatorios (
 	autor character varying(255) NOT NULL,
 	assunto character varying(255) NOT NULL,
 	data_hora timestamp NOT NULL,
 	id SERIAL NOT NULL PRIMARY KEY
-)
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.racas (
+	descricao character varying(255),
+	codigo integer NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.especies (
+	descricao character varying(255),
+	codigo integer NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.produtos (
+	tipo character varying(255) NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS clinivetschema.venda (
+	id_consulta integer NOT NULL,
+	id_produto integer NOT NULL
+);
+
+ALTER TABLE ONLY clinivetschema.animais
+	ADD CONSTRAINT animais_fk FOREIGN KEY (id_dono) REFERENCES clinivetschema.clientes(id);
+
+
