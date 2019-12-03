e6c3e4aa42ad1b6a041274754f61e6443e55f6cd
igorbraga <igorbgalvan@hotmail.com>
2019-12-02 23:43:39 -0400

    DAOS Atualizada

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -4,7 +4,9 @@ import java.sql.Timestamp;
 
 public class Administrador extends Funcionario {
 
-    public Administrador(String nome, char sexo, String endereco, String cidade, String estado, String telefoneResidencial, String telefoneCelular, String email, Timestamp dataDeHoje, int id, String cpf) {
+    public Administrador(String nome, String sexo, String endereco, String cidade, 
+    		String estado, String telefoneResidencial, String telefoneCelular, String email, 
+    		String dataDeHoje, int id, String cpf) {
     	super(nome, sexo, endereco, cidade, estado, telefoneResidencial, telefoneCelular, email, dataDeHoje, id, cpf);
     }
 
