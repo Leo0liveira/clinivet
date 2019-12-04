cbb3c09794b9d9ffb73c36933056d9c9501f2aef
Gabriel Borges <gabriel.bgs00@gmail.com>
2019-12-04 12:20:14 -0400

    Add files via upload

--- src/visao/FuncionarioCRUD.java
+++ src/visao/FuncionarioCRUD.java
@@ -1,5 +1,8 @@
 package visao;
 
+import controle.ControleFuncionario;
+import modelo.Funcionario;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -51,6 +54,9 @@ public class FuncionarioCRUD {
     private JPanel remove_error_2;
     private JPanel mainPanel;
     private JPanel remove_panel;
+    private Funcionario f;
+    private ControleFuncionario cf;
+    private int matricula;
 
     public FuncionarioCRUD() {
         frame = new JFrame("RaçaCRUD");
@@ -58,7 +64,8 @@ public class FuncionarioCRUD {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(400, 500);
+        frame.setSize(400, 600);
+        cf = new ControleFuncionario();
 
         //Definoções
         alt_panel.setVisible(false);
@@ -73,19 +80,42 @@ public class FuncionarioCRUD {
         adicionarFuncionarioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                if (verificaCampos(tfnome.getText(), tfcidade.getText(), tfcpf.getText(), tfdatacont.getText(), tfendereco.getText(), tfestado.getText(), tftelres.getText(), tfrelcel.getText(), tfemail.getText())) {
+                    if (cf.adicionaFuncionario(tfnome.getText(), tfendereco.getText(), tfcidade.getText(), tfestado.getText(), tftelres.getText(), tfrelcel.getText(), tfemail.getText(), tfdatacont.getText(), tfcpf.getText(), addpermbox.getActionCommand())) {
+                        JOptionPane.showMessageDialog(mainPanel, "Funcionário Adicionado com sucesso");
+                    } else {
+                        JOptionPane.showMessageDialog(mainPanel, "Não foi possivel cadastrar o funcionário, por favor reveja os campos e tente novamente");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(mainPanel, "informações faltando, por favor reveja os campos e tente novamente");
+                }
             }
         });
         alterarFuncionarioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                alt_panel.setVisible(false);
+                alt_error_2.setVisible(false);
+                if (verificaCampos(tf_alt_cidade.getText(), tf_alt_cpf.getText(), tf_alt_datacon.getText(), tf_alt_email.getText(), tf_alt_endereco.getText(), tf_alt_estado.getText(), tf_alt_nome.getText(), tf_alt_telcel.getText(), tf_alt_telres.getText())) {
+                    if (cf.alteraFuncionario(matricula, tf_alt_nome.getText(), tf_alt_endereco.getText(), tf_alt_cidade.getText(), tf_alt_estado.getText(), tf_alt_telres.getText(), tf_alt_telcel.getText(), tf_alt_email.getText(), tf_alt_datacon.getText(), tf_alt_cpf.getText(), altpermbox.getActionCommand())) {
+                        JOptionPane.showMessageDialog(mainPanel, "Alteração realizada com sucesso");
+                    } else {
+                        alt_error_2.setVisible(true);
+                    }
+                } else {
+                    alt_error_2.setVisible(true);
+                }
             }
         });
         removerFuncionarioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                remove_error_2.setVisible(false);
+                if (cf.removeFuncionario(matricula)){
+                    JOptionPane.showMessageDialog(mainPanel, "Remoção realizada com sucesso");
+                } else{
+                    remove_error_2.setVisible(true);
+                }
             }
         });
         voltarButton.addActionListener(new ActionListener() {
@@ -98,17 +128,46 @@ public class FuncionarioCRUD {
         irButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                alt_error.setVisible(false);
+                matricula = Integer.parseInt(tf_alt_cpf.getText());
+                f = cf.buscaFuncionario(matricula);
+                if (f != null) {
+                    tf_alt_cidade.setText(f.getCidade());
+                    tf_alt_cpf.setText(f.getCpf());
+                    tf_alt_datacon.setText(f.getData_contratacao());
+                    tf_alt_email.setText(f.getEmail());
+                    tf_alt_endereco.setText(f.getEndereco());
+                    tf_alt_estado.setText(f.getEstado());
+                    tf_alt_nome.setText(f.getNome());
+                    tf_alt_telres.setText(f.getTelefone_residencial());
+                    tf_alt_telcel.setText(f.getTelefone_celular());
+                    alt_panel.setVisible(true);
+                } else {
+                    alt_error.setVisible(true);
+                }
             }
         });
         irButton2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                //todo
-
-
+                matricula = Integer.parseInt(tf_rem_cpf.getText());
+                f = cf.buscaFuncionario(matricula);
+                if (f == null) {
+                    remove_error.setVisible(true);
+                } else {
+                    cf.removeFuncionario(matricula);
+                    JOptionPane.showMessageDialog(mainPanel, "Funcionário removido");
+                }
             }
         });
     }
+
+    private boolean verificaCampos(String campo1, String campo2, String campo3, String campo4, String campo5, String campo6, String campo7, String campo8, String campo9) {
+        if (campo1 == null || campo2 == null || campo3 == null || campo4 == null || campo5 == null || campo6 == null || campo7 == null || campo8 == null || campo9 == null) {
+            return false;
+        }
+        return true;
+    }
+
     //senha
 }
