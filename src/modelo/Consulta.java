64d1cfe085559472da0efc5d726b983769ef6b84
Robson Pimenta Moura Junior <43676289+RobsonPMJ@users.noreply.github.com>
2019-12-04 00:12:21 -0400

    Alteração

--- src/modelo/Consulta.java
+++ src/modelo/Consulta.java
@@ -6,11 +6,16 @@ public class Consulta extends Procedimento{
 
     private int idAnimal;
     private int idVeterinario;
+    private String horario;
+    private String pagamentoForma;
 
-    public Consulta(int idAnimal, int idVeterinario, String tipo, Date dataHora, int id) {
+    public Consulta(int idAnimal, int idVeterinario, String tipo, Date dataHora, int id, String horario, String pagamentoForma) {
         super(tipo, dataHora, id);
         this.idAnimal = idAnimal;
         this.idVeterinario = idVeterinario;
+        this.horario = horario;
+        this.pagamentoForma = pagamentoForma;
+        
     }
 
     public int getIdAnimal() {
@@ -36,5 +41,22 @@ public class Consulta extends Procedimento{
     public void finalizarConsulta() {
         //TODO: implementar
     }
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	public String getPagamentoForma() {
+		return pagamentoForma;
+	}
+
+	public void setPagamentoForma(String pagamentoForma) {
+		this.pagamentoForma = pagamentoForma;
+	}
+    
 
 }
--- src/visao/interfaces/IConsultarAnimais.java
+++ src/visao/interfaces/IConsultarAnimais.java
@@ -3,6 +3,6 @@ package visao.interfaces;
 import java.util.Date;
 
 public interface IConsultarAnimais {
-    public boolean salvarDados(int idAnimal, int idVeterinario, String tipo, Date data, String horario);
+    public boolean salvarDados(int idAnimal, int idVeterinario, String tipo, Date data, String horario, String pagamentoForma);
 
 }
