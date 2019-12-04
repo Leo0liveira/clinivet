827a7eba051aaf6dd2a0aaf22e60b0cc05b7bb86
Robson Pimenta Moura Junior <43676289+RobsonPMJ@users.noreply.github.com>
2019-12-04 01:13:47 -0400

    modelo.veterinario

--- src/modelo/Veterinario.java
+++ src/modelo/Veterinario.java
@@ -4,7 +4,7 @@ public class Veterinario extends Funcionario {
 
     public Veterinario(String nome, String sexo, String endereco, String cidade, String estado, String telefoneResidencial,
                        String telefoneCelular, String email, String dataHora, int id, String cpf) {
-        super(nome, sexo, endereco, cidade, estado, telefoneResidencial, telefoneCelular, email, dataHora, id, cpf);
+        super(id, sexo, endereco, cidade, estado, telefoneResidencial, telefoneCelular, email, dataHora, nome, cpf);
     }
 
 
