297f443facdf480b692720fc16235f790898f821
Robson Pimenta Moura Junior <43676289+RobsonPMJ@users.noreply.github.com>
2019-12-03 23:24:44 -0400

    Alteração ControleAnimal

--- src/controle/ControleAnimal.java
+++ src/controle/ControleAnimal.java
@@ -9,71 +9,58 @@ public class ControleAnimal {
     public Animal animal;
     private ResultSet resultSet;
 
-    public boolean adicionaAnimal(String nome, int proprietario, String nascimento, int especie, int raca, String sexo, String cor)
-    {
+    public boolean adicionaAnimal(String nome, int proprietario, String nascimento, int especie, int raca, String sexo, String cor) {
         Animal animal = new Animal("", nome, proprietario, nascimento, especie, raca, sexo, cor);
 
-        if(AnimalDAO.cadastrar(animal))
-        {
+        if (AnimalDAO.cadastrar(animal)) {
             return true;
-        }
-        else 
-        {
+        } else {
             return false;
         }
     }
 
-    public boolean alteraAnimal(int codigo, String nome, int proprietario, String nascimento, int especie, int raca, String sexo, String cor)
-    {
+    public boolean alteraAnimal(int codigo, String nome, int proprietario, String nascimento, int especie, int raca, String sexo, String cor) {
         Animal animal = new Animal(codigo, nome, proprietario, nascimento, especie, raca, sexo, cor);
 
         try {
-			if(AnimalDAO.alterar(animal))
-			{
-			    return true;
-			}
-			else 
-			{
-			    return false;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-
-    public boolean removeAnimal(int codigo)
-    {
-        try {
-			if(AnimalDAO.remover(codigo)) 
-			{
-			    return true;
-			}
-			else 
-			{
-			    return false;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-
-    public boolean recuperar(Animal animal)
-        {
-            try{
-                resultSet = (ResultSet) AnimalDAO.recuperar(Integer.valueOf(animal.getCodigo()));
-                animal.setCodigo(resultSet.getInt(1));
-                animal.setNome(resultSet.getString(1));
-                animal.setProprietario(resultSet.getInt(2));
-                animal.setNascimento(resultSet.getString(2));
-                animal.setEspecie(resultSet.getInt(3));
-                animal.setRaca(resultSet.getInt(4));
-                animal.setSexo(resultSet.getString(3));
-                animal.setCor(resultSet.getString(4));
-
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (AnimalDAO.alterar(animal)) {
+                return true;
+            } else {
+                return false;
             }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+    }
+
+    public boolean removeAnimal(int codigo) {
+        try {
+            if (AnimalDAO.remover(codigo)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public Animal buscaAnimal(int codigo) {
+        Animal animal = null;
+        ResultSet rs;
+        try {
+            rs = AnimalDAO.recuperar(Codigo);
+            while (rs.next()) {
+                animal = new Animal(rs.getInt("codigo"), rs.getString("nome"), rs.getInt("proprietario"), rs.getString("nascimento"), rs.getInt("especie"), rs.getInt("raca"), rs.getString("sexo"), rs.getString("cor"))
+            }
+        } catch (ClassNotFoundException | SQLException | NaoEncontradoExeception e) {
+            return null;
+        }
+        if (animal == null)
+            return null;
+
+        return animal;
+    }
 }
